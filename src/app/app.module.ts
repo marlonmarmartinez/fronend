b6f7d706a8c63d9665c70eda8062fda82d059f3e
capo <algoloco@gmail.com>
2018-09-12 09:17:23 -0500

    mi proyecto esta funcionando, se creo el modulo routing y el componente usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
